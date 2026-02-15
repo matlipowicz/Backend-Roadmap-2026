026d8351da54e14e85b3530a91d8a6ef149aef13
Mateusz Lipowicz <lipusek1997@gmail.com>
2026-02-15 16:30:46 +0100

    build: setup gh pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  base: "/Backend-Roadmap-2026",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
